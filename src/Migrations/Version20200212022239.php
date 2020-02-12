fb6d23f0baffaef4f1339f3210294e6a89378c75
Yorch Peraza <jorge@colibricode.com>
2020-02-11 20:28:00 -0600

    Add user bases

---
+++ src/Migrations/Version20200212022239.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200212022239 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user ADD user_name VARCHAR(180) NOT NULL, ADD image VARCHAR(255) NOT NULL, ADD full_name VARCHAR(255) NOT NULL, ADD job_title VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D64924A232CF ON user (user_name)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_8D93D64924A232CF ON user');
+        $this->addSql('ALTER TABLE user DROP user_name, DROP image, DROP full_name, DROP job_title');
+    }
+}
