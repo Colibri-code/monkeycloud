049337365ab41e1172d7d4df57014cb565711020
Yorch Peraza <jorge@colibricode.com>
2020-04-29 17:58:33 -0600

    property access

--- src/Controller/GitFlowController.php
+++ src/Controller/GitFlowController.php
@@ -6,6 +6,11 @@ use App\Service\GitRepo;
 use Symfony\Component\HttpFoundation\Response;
 use symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
+use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
+
 
 class GitFlowController
 {
@@ -14,13 +19,20 @@ class GitFlowController
      * @Route("/git/ejemplo", name="app_git")
      */
     public function showGit(){
-        
 
         $showGit = new GitRepo;
-        $showGitBranches = $showGit->GitRepoLastTag('../');
-        
+        $showGitBranches = $showGit->GitRepoBranches('../');
+        $reflectionExtractor = new ReflectionExtractor();
+        $phpDocExtractor = new PropertyInfoExtractor(
+            [$reflectionExtractor,
+                ]
+        );
+
+        $properties = $phpDocExtractor->getProperties(get_class($showGitBranches['0']->getRepository()->getCaller()));
+
         return new Response(
-            '<html><body>Git Status: '. print_r($showGitBranches).'</body></html>'
+            '<html><body>Git Status: '. print_r($properties).'</body></html>'
+
         );
     }
 
