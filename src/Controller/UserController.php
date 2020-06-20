093ff28e3c6f27304c9edb134aa27a9414ddc311
Diego Avila <diego.avila@colibricode.com>
2020-06-20 11:00:02 -0600

    implemented post for User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,14 +4,31 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/user", name="user", methods={"POST"})
      */
-    public function index()
+    public function add(Request $request): JsonResponse
     {
-        
-    }
+        $data = json_decode($request->getContent(), true);
+
+        $email = $data['email']; 
+        $password = $data['password'];
+        $FullName = $data['FullName'];
+        $UserName = $data['UserName'];
+
+        if(empty($email) || empty($password)||empty($FullName) || empty($UserName) ){
+
+                throw new NotFoundHttpException('Expectig mandatory parameters!');
+
+        }
+
+        $this->UserRepository->newUser($email,$password,$FullName,$UserName);
+
+        return new JsonResposne(['status' => 'Success'], Response::HTTP_CREATED);
+
+    }   
 }
