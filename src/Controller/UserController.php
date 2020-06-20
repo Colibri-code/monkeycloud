e3e62f61b1763aba1e39b7ae76b8dc8fd1260b97
Diego Avila <diego.avila@colibricode.com>
2020-06-20 10:10:21 -0600

    started development of controller for api to deal with database queries

---
+++ src/Controller/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="user")
+     */
+    public function index()
+    {
+        
+    }
+}
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
-use Doctrine\ORM\ENtityManagerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 /**
